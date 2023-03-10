90dc105d534b687fa04901e47174dc8e456a6ecb
jshvaron <jshva@icloud.com>
2023-03-09 20:59:28 -0600

    added dependencies and user.init to the user.js

---
+++ models/users.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const sequalize = require('../connection');
+const { DataTypes } = require('sequelize/types');
+
+
+// Creates a new sequalize model for Users
+class User extends Model {
+// checkPassword(loginPw){
+//     return bcrypt.compareSync(loginPw, this.password);
+}
+
+
+User.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate:{
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8,64],
+            },
+        },
+    },
+
+);
