49b52512209a05a9351e813ab2843be2fa29fa9e
jshvaron <jshva@icloud.com>
2023-03-09 21:10:25 -0600

    Hooks and sequalize attributes have been added to the user.js

--- models/users.js
+++ models/users.js
@@ -41,5 +41,19 @@ User.init(
             },
         },
     },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash
+                (newUserData.password, 10);
+                return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    },
 
 );
