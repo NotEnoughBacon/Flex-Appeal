8802b95c2ff95b432a52ab134d077b706f83cb79
valerierobles <roblesv08@gmail.com>
2023-03-15 20:29:18 -0500

    workout routes

---
+++ controllers/api/workouts.js
@@ -0,0 +1,36 @@
+const router = require('express').Router(); 
+const { Workout, Exercise } = require('../models');
+
+// GET all workouts
+router.get('/', (req, res) => {
+    console.log('======================');
+    Workout.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbWorkoutData => res.json(dbWorkoutData))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+    }); 
+
+// GET a single workout
+router.get('/:id', (req, res) => {
+    Workout.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+    })
+        .then(dbWorkoutData => {
+            if (!dbWorkoutData) {
+                res.status(404).json({ message: 'No workout found with this id' });
+                return;
+            }
+            res.json(dbWorkoutData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+}); 
