d68972fdd491da58ffd9c1ba3e56c472b313ac2d
jshvaron <jshva@icloud.com>
2023-03-15 20:46:39 -0500

    assigned ids and created tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,20 @@
 DROP DATABASE IF EXISTS workout_db;
 
 CREATE DATABASE workout_db;
+
+
+CREATE TABLE workout (
+    id INT NOT NULL,
+    workout_name VARCHAR(30),
+    dworkout_description VARCHAR(30),
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE movements (
+    id INT NOT NULL,
+    move_name VARCHAR(30),
+    move_sets VARCHAR(30),
+    move_reps VARCHAR(30),
+    FOREIGN KEY (workout_id),
+    REFERENCES workout(id),
+);
--- seeds/Workouts.js
+++ seeds/Workouts.js
@@ -2,9 +2,21 @@ const { Workouts } = require('../models');
 
 const workoutData = [
     {
-        name: 'legs 1',
-        description: ''
-    }
+        id: [1],
+        name: 'Push Workout',
+        description: 'burpees, jumping jacks,  mountain climbers, pushups, overhead press, bench press, dips, tricep extensions, crunches, leg raises. '
+    },
+    { 
+        id: [2],
+        name: 'Pull Workout',
+        description : 'side to side hops, bent over rows, pullups, rows, deadlifts, Romanian Deadlifts, planks, side planks, bicep curls. '
+    },
+    { 
+        id: [3],
+        name: 'Legs Workout',
+        description : 'high knees, skaters, squats, lunges, reverse lunges, squat jumps, jumping lunges, hip thrusts, hip bridges, side lunges.'
+    },
+
 ];
 
 async function seedWorkouts() {
