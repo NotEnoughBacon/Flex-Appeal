2a2427dd8786f9ae6d9dc540a7d0f8ebc8bdcfe6
TJ Momon <tjmomon@TJs-MacBook-Pro.attlocal.net>
2023-03-16 20:39:50 -0500

    began work on handlebars template

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,11 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.use(express.static('public'))
+// app.get('/', (req, res) => res.send('test'));
+// app.listen(port, () => console.log(`App listening to port ${port}`));
+
 class DigitalClock {
     constructor(element) {
         this.element = element;
